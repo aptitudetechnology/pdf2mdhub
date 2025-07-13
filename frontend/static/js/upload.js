95843f58f4da4143be4eddccb2217bb1c9edf036
Christopher Caston <chris@caston.id.au>
2025-07-13 17:36:35 +0800

    fixed upload.js

--- frontend/static/js/upload.js
+++ frontend/static/js/upload.js
@@ -196,7 +196,7 @@ class UploadInterface {
 
                 // Send the file and markdown content to the backend
                 const formData = new FormData();
-                #formData.append('pdf_file', file);
+                //formData.append('pdf_file', file);
                 formData.append('file', file); // Change 'pdf_file' to 'file'
                 formData.append('markdown_content', markdownContent);
                 formData.append('tags', tagsJsonString); // Append the JSON stringified tags
