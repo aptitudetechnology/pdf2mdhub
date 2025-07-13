4795857f617866da8f094511b2885573a85bdad3
Christopher Caston <chris@caston.id.au>
2025-07-13 22:44:50 +0800

    updated document.py

--- backend/models/tag.py
+++ backend/models/tag.py
@@ -1,4 +1,5 @@
-from backend.models import db
+# backend/models/tag.py
+from backend.models import db # Import the db instance
 
 class Tag(db.Model):
     __tablename__ = 'tags'
@@ -6,8 +7,18 @@ class Tag(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True, nullable=False)
 
+    # *** ADD THIS RELATIONSHIP DEFINITION ***
+    documents = db.relationship(
+        'Document',             # The name of the related model class
+        secondary='document_tags', # The string name of the association table defined in document.py
+        back_populates='tags'   # The name of the relationship on the Document model
+    )
+
     def to_dict(self):
         return {
             'id': self.id,
             'name': self.name
         }
+
+    def __repr__(self):
+        return f'<Tag {self.name}>'
