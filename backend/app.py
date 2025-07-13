543a359c4402ce692ec4a8c66fa7b6636ed11bc6
Christopher Caston <chris@caston.id.au>
2025-07-13 16:54:44 +0800

    update appy.py

--- backend/app.py
+++ backend/app.py
@@ -400,4 +400,4 @@ if __name__ == '__main__':
     # For initial dev, create_all() is fine.
     with app.app_context():
         db.create_all()
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5050)
