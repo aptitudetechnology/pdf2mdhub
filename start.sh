655b28c1135e1626e70ce5c1a46adab979d1368a
Christopher Caston <chris@caston.id.au>
2025-07-15 12:35:11 +0800

    added webcontainer demo

--- start.sh
+++ start.sh
@@ -56,5 +56,5 @@ echo "Starting Flask server..."
 # Use 'flask run' which correctly sets up the application context
 # Add --host 0.0.0.0 to make it accessible from outside localhost.
 # Add --port 5050 if you want a specific port (default is 5000)
-flask run --host 0.0.0.0 --port 5050
+#flask run --host 0.0.0.0 --port 5050
 flask run --host=0.0.0.0 --port=5050 --cert=backend/ssl/cert.pem --key=backend/ssl/key.pem
