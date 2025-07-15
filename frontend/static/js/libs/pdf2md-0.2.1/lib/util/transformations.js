d9df1e6c532cf484f5f1b4c98e7493ccee12e5b5
Christopher Caston <chris@caston.id.au>
2025-07-15 21:54:13 +0800

    added webcontainer demo

--- frontend/static/js/libs/pdf2md-0.2.1/lib/util/transformations.js
+++ frontend/static/js/libs/pdf2md-0.2.1/lib/util/transformations.js
@@ -1,21 +1,27 @@
-const CalculateGlobalStats = require('../models/transformations/text-item/CalculateGlobalStats')
+// util/transformations.js - Refactored to ES Module Syntax
 
-const CompactLines = require('../models/transformations/line-item/CompactLines')
-const RemoveRepetitiveElements = require('../models/transformations/line-item/RemoveRepetitiveElements')
-const VerticalToHorizontal = require('../models/transformations/line-item/VerticalToHorizontal')
-const DetectTOC = require('../models/transformations/line-item/DetectTOC')
-const DetectListItems = require('../models/transformations/line-item/DetectListItems')
-const DetectHeaders = require('../models/transformations/line-item/DetectHeaders')
+// Import all required transformation classes and ParseResult
+// IMPORTANT: Remember to add the .js extension for all relative imports!
 
-const GatherBlocks = require('../models/transformations/line-item-block/GatherBlocks')
-const DetectCodeQuoteBlocks = require('../models/transformations/line-item-block/DetectCodeQuoteBlocks')
-const DetectListLevels = require('../models/transformations/line-item-block/DetectListLevels')
-const ToTextBlocks = require('../models/transformations/ToTextBlocks')
-const ToMarkdown = require('../models/transformations/ToMarkdown')
+import CalculateGlobalStats from '../models/transformations/text-item/CalculateGlobalStats.js';
 
-const ParseResult = require('../models/ParseResult')
+import CompactLines from '../models/transformations/line-item/CompactLines.js';
+import RemoveRepetitiveElements from '../models/transformations/line-item/RemoveRepetitiveElements.js';
+import VerticalToHorizontal from '../models/transformations/line-item/VerticalToHorizontal.js';
+import DetectTOC from '../models/transformations/line-item/DetectTOC.js';
+import DetectListItems from '../models/transformations/line-item/DetectListItems.js';
+import DetectHeaders from '../models/transformations/line-item/DetectHeaders.js';
 
-exports.makeTransformations = fontMap => [
+import GatherBlocks from '../models/transformations/line-item-block/GatherBlocks.js';
+import DetectCodeQuoteBlocks from '../models/transformations/line-item-block/DetectCodeQuoteBlocks.js';
+import DetectListLevels from '../models/transformations/line-item-block/DetectListLevels.js';
+import ToTextBlocks from '../models/transformations/ToTextBlocks.js';
+import ToMarkdown from '../models/transformations/ToMarkdown.js';
+
+import ParseResult from '../models/ParseResult.js';
+
+// Change exports.makeTransformations to a named export
+export const makeTransformations = fontMap => [
   new CalculateGlobalStats(fontMap),
   new CompactLines(),
   new RemoveRepetitiveElements(),
@@ -30,17 +36,18 @@ exports.makeTransformations = fontMap => [
 
   new ToTextBlocks(),
   new ToMarkdown(),
-]
+];
 
-exports.transform = (pages, transformations) => {
-  var parseResult = new ParseResult({ pages })
-  let lastTransformation
+// Change exports.transform to a named export
+export const transform = (pages, transformations) => {
+  var parseResult = new ParseResult({ pages });
+  let lastTransformation;
   transformations.forEach(transformation => {
     if (lastTransformation) {
-      parseResult = lastTransformation.completeTransform(parseResult)
+      parseResult = lastTransformation.completeTransform(parseResult);
     }
-    parseResult = transformation.transform(parseResult)
-    lastTransformation = transformation
-  })
-  return parseResult
-}
+    parseResult = transformation.transform(parseResult);
+    lastTransformation = transformation;
+  });
+  return parseResult;
+};
