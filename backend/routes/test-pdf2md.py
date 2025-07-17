bd7f829540ed70542417010b36959d8e985606af
Christopher Caston <chris@caston.id.au>
2025-07-17 14:20:51 +0800

    added test route

--- backend/routes/test-pdf2md.py
+++ backend/routes/test-pdf2md.py
@@ -2,9 +2,9 @@
 
 from flask import Blueprint, render_template
 
-webcontainer_demo_bp = Blueprint('test-pdf2md, __name__)
+test-pdf2md_bp = Blueprint('test-pdf2md, __name__)
 
-@webcontainer_demo_bp.route('/test-pdf2md')
-def webcontainer_demo():
-    # Assumes you have 'webcontainer-demo.html' in frontend/templates/
+@test-pdf2md_bp.route('/test-pdf2md')
+def test-pdf2md():
+    # Assumes you have 'test-pdf2md.html' in frontend/templates/
     return render_template('test-pdf2md.html')
